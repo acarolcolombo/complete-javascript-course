67d2bcd96672bd6f41940ddc5188c7d5ab2ad3d1
Ana Caroline Colombo <acarolcolombo@gmail.com>
2021-06-11 17:15:19 +0100

    Section 2 - Lesson 26

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -103,3 +103,28 @@ if(language != "English" && population > 50 && !isIsland){
 } else {
     console.log(`${country} does not meet your criteria :(`);
 }
+
+//SWITCH STATEMENT
+
+//language = "mandarim";
+
+switch(language) {
+    case "chinese":
+    case "mandarim":
+        console.log("MOST number of native speakers!");
+        break;
+    case "spanish":
+        console.log("2nd place in number of natve speakers");
+        break;
+    case "english":
+        console.log("3rd place");
+        break;
+    case "hindi":
+        console.log("number 4");
+        break;
+    case "arabic":
+        console.log("5th most spoken language");
+        break;
+    default:
+        console.log("Great language too =)");
+}
